a27525f276fa215cf9e1f424399ab0216136434f
Corey Kepple <corey@bounceexchange.com>
2018-03-03 20:39:44 -0500

    Update to users models

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+const connection = require('../helpers/dbConnection');
+const Sequelize = require( 'sequelize' );
+
+var User = connection.define('User', {
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING
+}, {});
+
+User.associate = function(models) {
+  // associations can be defined here
 };
+
+module.exports = User;
