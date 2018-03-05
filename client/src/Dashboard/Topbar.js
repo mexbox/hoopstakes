ebaaeb2f294c801d0d7cba0449b474f2454d01a7
Corey Kepple <corey@bounceexchange.com>
2018-03-04 20:49:33 -0500

    Refactor Topbar, include useravatar

--- client/src/Dashboard/Topbar.js
+++ client/src/Dashboard/Topbar.js
@@ -3,40 +3,38 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import classNames from 'classnames';
 
-//Top Bar
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
-import Logo from '../assets/images/logo.png'
+import UserAvatar from './UserAvatar';
+import Logo from '../assets/images/logo.png';
 
 const styles = theme => ({
-    appBar: {
-        position: 'absolute',
+    flex: {
+        flex: 1,
     },
-
     logo: {
         maxHeight: '50px',
         padding: '0px 10px',
         cursor: 'pointer'
     }
-});
+}); 
 
 class Topbar extends React.Component {
-
     render() {
         const { classes } = this.props;
-
+        
         return (
-            <AppBar className={classNames(classes.appBar)}>
-                <Toolbar disableGutters={true}>
-                    <img alt="Hoopstakes Logo" src={Logo} className={classNames(classes.logo)} onClick={this.props.logOut} />
-                    <Typography variant="title" color="inherit" noWrap className={classes.tournamentName}>
-                        Hoopstakes
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-      
+            <div className={classes.root}>
+                <AppBar position="absolute">
+                    <Toolbar disableGutters={true}>
+                        <img alt="Hoopstakes Logo" src={Logo} className={classNames(classes.logo)} />
+                        <Typography variant="title" color="inherit" noWrap className={classes.flex}>Hoopstakes</Typography>
+                        <UserAvatar logOut={this.props.logOut} />
+                    </Toolbar>
+                </AppBar>
+            </div>
         );
     }
 }
