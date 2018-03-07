fcc877ebce47d8a1b0b1750ad46fb786f08a448c
Corey Kepple <corey@bounceexchange.com>
2018-03-06 23:19:32 -0500

    Add nbastatid to team model

--- migrations/20180306161920-create-team.js
+++ migrations/20180306161920-create-team.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      nbaStatId: {
+        type: Sequelize.INTEGER
+      },
       name: {
         type: Sequelize.STRING
       },
