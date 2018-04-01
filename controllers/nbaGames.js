83381be8c9e47a73ab731fb9eca0275592b8c389
Corey Kepple <corey@bounceexchange.com>
2018-04-01 00:43:44 -0400

    remove need to pass models into methods

--- controllers/nbaGames.js
+++ controllers/nbaGames.js
@@ -2,14 +2,11 @@
 
 const models = require('../models/dbConnection');
 const NbaGameModel = models.NbaGame;
-const NbaTeams = models.NbaTeam;
-
-var debug = require('debug')('hoopstakes:nbaGames');
 
 const Games = { 
     getAll: async (req, res) =>{
         try{
-          const allGames = await NbaGameModel.getGamesByDay(NbaTeams);
+          const allGames = await NbaGameModel.getGamesByDay();
           return res.status(201).send(allGames);
         } catch (error) {
           debug(error);
